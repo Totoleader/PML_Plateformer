d862b30dbea393cd341faa53fe07a5018b288bc6
Maxil Côté <maxilcote@gmail.com>
2022-07-09 17:17:21 -0400

    First version of basic player movement

--- Assets/Scipts/Player/PlayerMovement.cs
+++ Assets/Scipts/Player/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float LeftRightInput;
+    public float speed;
     
     // Start is called before the first frame update
     void Start()
@@ -14,6 +16,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       //left-right movement
+       LeftRightInput = Input.GetAxis("Horizontal");    
+       GetComponent<Rigidbody2D> ().velocity = new Vector2 (speed * LeftRightInput, GetComponent<Rigidbody2D> ().velocity.y);
+       
+       //jump
+       
     }
 }
