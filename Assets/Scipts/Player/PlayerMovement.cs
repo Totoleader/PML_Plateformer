51eb7ca0da639e7fb3f70b42a8ac7751e7571506
Maxil Côté <maxilcote@gmail.com>
2022-07-11 19:14:05 -0400

    ajustements level et player controls

--- Assets/Scipts/Player/PlayerMovement.cs
+++ Assets/Scipts/Player/PlayerMovement.cs
@@ -5,17 +5,19 @@ namespace Scipts.Player
     public class PlayerMovement : MonoBehaviour
     {
         private float leftRightInput;
-        public float speed;
-        public float jumpForce;
+        [SerializeField] private float speed;
+        [SerializeField] private float jumpForce;
         private Rigidbody2D rb;
 
         private Animator anim;
+        private SpriteRenderer sprite;
 
         // Start is called before the first frame update
         void Start()
         {
             rb = GetComponent<Rigidbody2D>();
             anim = GetComponent<Animator>();
+            sprite = GetComponent<SpriteRenderer>();
         }
 
         // Update is called once per frame
@@ -45,11 +47,13 @@ namespace Scipts.Player
                         if (leftRightInput > 0)
                         {
                             anim.SetBool("Running", true); //st animation to running right
+                            sprite.flipX = false;
                         }
                         //check if player is moving left
                         else if (leftRightInput < 0)
                         {
                             anim.SetBool("Running", true); //set animation to running left
+                            sprite.flipX = true;
                         }
                         //check if player is idle
                         else
