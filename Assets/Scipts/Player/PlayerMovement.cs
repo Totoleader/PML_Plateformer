45074710240fe0644902221ae920e8c568af7711
Maxil Côté <maxilcote@gmail.com>
2022-07-11 18:15:16 -0400

    Amélioration du déplacement, implémentation du suat et début des animations

--- Assets/Scipts/Player/PlayerMovement.cs
+++ Assets/Scipts/Player/PlayerMovement.cs
@@ -1,26 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+namespace Scipts.Player
 {
-    private float LeftRightInput;
-    public float speed;
-    
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerMovement : MonoBehaviour
     {
-        
-    }
+        private float leftRightInput;
+        public float speed;
+        public float jumpForce;
+        private Rigidbody2D rb;
 
-    // Update is called once per frame
-    void Update()
-    {
-       //left-right movement
-       LeftRightInput = Input.GetAxis("Horizontal");    
-       GetComponent<Rigidbody2D> ().velocity = new Vector2 (speed * LeftRightInput, GetComponent<Rigidbody2D> ().velocity.y);
-       
-       //jump
-       
+        private Animator anim;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            anim = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            MovementUpdate();
+            AnimationUpdate();
+        }
+
+        void MovementUpdate()
+        {
+             //left-right movement
+             leftRightInput = Input.GetAxisRaw("Horizontal");
+             rb.velocity = new Vector2(speed * leftRightInput, rb.velocity.y);
+             
+             //jump
+             if (Input.GetButtonDown("Jump"))
+             {
+                 rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+             }
+        }
+
+        void AnimationUpdate()
+        {
+            //check if player is moving right
+                        if (leftRightInput > 0)
+                        {
+                            anim.SetBool("Running", true); //st animation to running right
+                        }
+                        //check if player is moving left
+                        else if (leftRightInput < 0)
+                        {
+                            anim.SetBool("Running", true); //set animation to running left
+                        }
+                        //check if player is idle
+                        else
+                        {
+                            anim.SetBool("Running", false); //set animation to idle
+                        }
+        }
     }
 }
