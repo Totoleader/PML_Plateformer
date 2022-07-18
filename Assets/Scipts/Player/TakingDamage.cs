27a5dd5af48b843816a5fd600e5331168218fb9e
Maxil Côté <maxilcote@gmail.com>
2022-07-18 17:21:26 -0400

    Hero taking damage script

---
+++ Assets/Scipts/Player/TakingDamage.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace Scipts.Player
+{
+    public class TakingDamage : MonoBehaviour
+    {
+        private Animator anim;
+
+       
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            anim = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("spike"))
+            {
+                Damaged();
+            }
+        }
+
+        private void Damaged()
+        {
+            anim.SetTrigger("damaged");
+        }
+    }
+}
